1d6c62fb2479e9bdfd87f8621ffa10eb8abb6956
Zoom-Developer <73240796+Zoom-Developer@users.noreply.github.com>
2022-05-16 17:54:12 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ RIOT_TOKEN = "RGAPI-XXXXXXXXXXXXXXXXX"
 DISCORD_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXX"
 LOG_CHANNEL = 0000000
 
-GAME_MODES = {"CLASSIC": "Ущелье призывателей", "ARAM": "Воющая пизда", "Туториал": "Туториал", "ONEFORALL": "ONEFORALL", "ASCENSION": "ASCENSION", "FIRSTBLOOD": "FIRSTBLOOD", "KINGPORO": "KINGPORO"}
+GAME_MODES = {"CLASSIC": "Ущелье призывателей", "ARAM": "Воющая бездна", "Туториал": "Туториал", "ONEFORALL": "Все за одного", "ASCENSION": "ASCENSION", "FIRSTBLOOD": "FIRSTBLOOD", "KINGPORO": "KINGPORO"}
 GAME_TYPES = {"CUSTOM_GAME": "Пользовательская игра", "MATCHED_GAME": "Игра с подбором игроков", "TUTORIAL_GAME": "Туториал"}
 
 ITEMS_EXCEPTIONS = [3031, 3089]
