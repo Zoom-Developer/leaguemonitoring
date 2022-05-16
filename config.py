4d2e9ab64dfd637b86ae3550e2955511c23bbd11
Zoom-Developer <73240796+Zoom-Developer@users.noreply.github.com>
2022-05-16 17:45:22 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+RIOT_TOKEN = "RGAPI-1aa24e86-acda-499c-a994-7006719fd995"
+DISCORD_TOKEN = "OTQxMzkxMjQwMjEyMTM1OTc4.YgVQ6Q.RaGp_r5ivnctNJr5Qfz_QZxKJ5E"
+LOG_CHANNEL = 0000000
+
+GAME_MODES = {"CLASSIC": "Ущелье призывателей", "ARAM": "Воющая пизда", "Туториал": "Туториал", "ONEFORALL": "ONEFORALL", "ASCENSION": "ASCENSION", "FIRSTBLOOD": "FIRSTBLOOD", "KINGPORO": "KINGPORO"}
+GAME_TYPES = {"CUSTOM_GAME": "Пользовательская игра", "MATCHED_GAME": "Игра с подбором игроков", "TUTORIAL_GAME": "Туториал"}
+
+ITEMS_EXCEPTIONS = [3031, 3089]
+
+TOP_VISUAL = {"kills": "", "assists": "", "kda": "", "goldEarned": " Gold", "totalDamageDealtToChampions": " Damage"}
+
+USERS = {
+    "TEST": {"id": "xxxxxxxxxxxxxxxxxxxxx"},
+}
